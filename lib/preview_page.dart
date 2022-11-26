61dc19732d30db543fb84f5c127561b06f538ac7
Soren Marcelino <soren.marcelino@airbus.com>
2022-11-26 21:19:03 +0100

    DetailVinPage with Tesseract response

--- lib/preview_page.dart
+++ lib/preview_page.dart
@@ -5,6 +5,11 @@ import 'tesseract_test_page.dart';
 
 import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'Vins.dart';
+import 'detail_vin_page.dart';
+
 class PreviewPage extends StatefulWidget {
   const PreviewPage({Key? key, required this.picture}) : super(key: key);
 
@@ -15,10 +20,37 @@ class PreviewPage extends StatefulWidget {
 }
 
 class _PreviewPageState extends State<PreviewPage> {
-
   bool _scanning = false;
   String _extractText = '';
   File _image = File('');
+
+  Future<http.Response> getVin(String _extractText) async {
+    var url = 'http://192.168.1.154:5000/api/vin/$_extractText';
+    var response = await http.get(Uri.parse(url.trim()));
+    final Map<String, dynamic> data = json.decode(response.body);
+    print("dataContentVin : $data");
+    print(data['nom']);
+    if (response.statusCode == 200) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => DetailVin(
+                  nom: data['nom'],
+                  descriptif: data['descriptif'],
+                  couleur: data['couleur'],
+                  embouteillage: data['embouteillage'],
+                  cepage: data['cepage'],
+                  chateau_domaine_propriete_clos:
+                  data['chateau_domaine_propriete_clos'],
+                  annee: data['annee'],
+                  prix: data['prix'],
+                  image_bouteille: data['image_bouteille'],
+                  url_producteur: data['url_producteur'])
+          ),
+      ); // To adapt
+    }
+    return response;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +60,8 @@ class _PreviewPageState extends State<PreviewPage> {
         child: Column(
           children: <Widget>[
             Column(mainAxisSize: MainAxisSize.min, children: [
-              Image.file(File(widget.picture.path), fit: BoxFit.cover, width: 250),
+              Image.file(File(widget.picture.path),
+                  fit: BoxFit.cover, width: 250),
               const SizedBox(height: 24),
               Text(widget.picture.path)
             ]),
@@ -48,15 +81,14 @@ class _PreviewPageState extends State<PreviewPage> {
                       _scanning = true;
                     });
                     _image = await File(widget.picture.path);
-                    _extractText = await FlutterTesseractOcr.extractText(_image.path, language: 'fra+eng', args: {
-                      "psm": "4",
-                      "preserve_interword_spaces": "1",
-                      //"textord_space_size_is_variable": ""
-                    });
+                    _extractText = await FlutterTesseractOcr.extractText(
+                        _image.path,
+                        language: 'fra+eng',
+                        args: {"psm": "4", "preserve_interword_spaces": "1"});
                     setState(() {
                       _scanning = false;
                     });
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => TesseractTestPage(title: "Inscription", extractedText: _extractText)));
+                    getVin(_extractText);
                   },
                 ),
               ],
